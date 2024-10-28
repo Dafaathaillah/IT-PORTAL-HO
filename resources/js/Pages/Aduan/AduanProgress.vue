59676535238d0f57cd87963419f21fbdd75fd056
dafaathaillah <dafabintang76@gmail.com>
2024-10-28 15:20:08 +0800

    aduan styling color badge status and disbale required image progress aduan

--- resources/js/Pages/Aduan/AduanProgress.vue
+++ resources/js/Pages/Aduan/AduanProgress.vue
@@ -479,7 +479,6 @@ const options = props.crew;
                                                 >Repair Image</label
                                             >
                                             <input
-                                                required
                                                 type="file"
                                                 ref="fileInput"
                                                 class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
