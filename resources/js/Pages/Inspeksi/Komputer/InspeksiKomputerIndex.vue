a2539c2b00109e8704474e94e1ee75cab30f8894
dafaathaillah <dafabintang76@gmail.com>
2024-11-13 13:31:42 +0800

    done view inspection komputer

--- resources/js/Pages/Inspeksi/Komputer/InspeksiKomputerIndex.vue
+++ resources/js/Pages/Inspeksi/Komputer/InspeksiKomputerIndex.vue
@@ -82,50 +82,58 @@ const handleFileUpload = (event) => {
     file.value = event.target.files[0];
 };
 
-const submitCsv = () => {
-    let timerInterval;
-    Swal.fire({
-        title: "Mengimport Data...",
-        text: "Mohon tunggu sebentar...",
-        allowOutsideClick: false,
-        didOpen: () => {
-            Swal.showLoading();
-        },
-    });
+const getBadgeClassStatusInspeksi = (status) => {
+    return status === "Y"
+        ? "bg-gradient-to-tl from-emerald-500 to-teal-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white"
+        : "bg-gradient-to-tl from-red-500 to-orange-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white";
+};
 
-    const formx = useForm({
-        file: file.value,
-    });
+const getBadgeTextStatusInspeksi = (status) => {
+    return status === "Y" ? "INSPECTED" : "NOT INSPECTED YET";
+};
 
-    function reloadPage() {
-        window.location.reload();
+const getBadgeClassStatusFindings = (status) => {
+    if (status === "OPEN") {
+        return "bg-gradient-to-tl from-cyan-500 to-sky-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white";
+    } else if (status === "CLOSED") {
+        return "bg-gradient-to-tl from-emerald-500 to-teal-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white";
+    } else {
+        return "bg-gradient-to-tl from-red-500 to-orange-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white"; // Default untuk status yang tidak dikenal
     }
+};
 
-    formx.post(route("printer.import"), {
-        onSuccess: () => {
-            // Show SweetAlert2 success notification
-            Swal.fire({
-                title: "Success!",
-                text: "Data has been successfully import!",
-                icon: "success",
-                confirmButtonText: "OK",
-                confirmButtonColor: "#3085d6",
-            });
+const getBadgeTextStatusFindings = (status) => {
+    if (status === "OPEN") {
+        return "OPEN";
+    } else if (status === "CLOSED") {
+        return "CLOSED";
+    } else {
+        return "NOT FINDINGS YET"; // Default teks untuk status yang tidak dikenal
+    }
+};
 
-            setTimeout(function () {
-                reloadPage();
-            }, 2000);
-        },
-        onError: () => {
-            Swal.fire({
-                title: "error!",
-                text: "Data not created!",
-                icon: "waring",
-                confirmButtonText: "OK",
-                confirmButtonColor: "#3085d6",
-            });
-        },
-    });
+const getBadgeClassStatusInventory = (status) => {
+    if (status === "READY_USED") {
+        return "bg-gradient-to-tl from-emerald-500 to-teal-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white";
+    } else if (status === "READY_STANBY") {
+        return "bg-gradient-to-tl from-yellow-500 to-yellow-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white";
+    } else if (status === "SCRAP") {
+        return "bg-gradient-to-tl from-red-500 to-orange-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white";
+    } else if (status === "BREAKDOWN") {
+        return "bg-gradient-to-tl from-rose-500 to-rose-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white";
+    }
+};
+
+const getBadgeTextStatusInventory = (status) => {
+    if (status === "READY_USED") {
+        return "READY_USED";
+    } else if (status === "READY_STANDBY") {
+        return "READY_STANDBY";
+    } else if (status === "SCRAP") {
+        return "SCRAP";
+    } else if (status === "BREAKDOWN") {
+        return "BREAKDOWN";
+    }
 };
 </script>
 
@@ -160,66 +168,10 @@ const submitCsv = () => {
         <div class="py-12">
             <div class="min-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex flex-wrap -mx-3">
-                    <!-- <form
-                        @submit.prevent="submitCsv"
-                        enctype="multipart/form-data"
-                    >
-                        <div class="flex">
-                            <div
-                                class="w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-0"
-                            >
-                                <div class="mb-4">
-                                    <input
-                                        type="file"
-                                        ref="fileInput"
-                                        enctype="multipart/form-data"
-                                        class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
-                                        @change="handleFileUpload"
-                                    />
-                                </div>
-                            </div>
-                            <div class="max-w-full shrink-0">
-                                <button
-                                    type="submit"
-                                    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                                >
-                                    <i class="fas fa-file-import"></i>
-                                    Import
-                                </button>
-                            </div>
-                            <div
-                                class="w-full max-w-full px-3 shrink-0 md:w-4/12 md:flex-0"
-                            >
-                                <a
-                                    href="/samplePrinter.xlsx"
-                                    download="Format-Import-Data-printer.xlsx"
-                                    target="_blank"
-                                    type="button"
-                                    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                                >
-                                    <i class="fas fa-download"></i>
-                                    Format Excel Data
-                                </a>
-                            </div>
-                        </div>
-                    </form> -->
-
                     <div class="flex-none w-full max-w-full px-3">
                         <div
                             class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border"
                         >
-                            <div
-                                class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent"
-                            >
-                                <Link
-                                    :href="route('printer.create')"
-                                    class="inline-block px-5 py-2.5 font-bold leading-normal text-center text-white align-middle transition-all bg-transparent rounded-lg cursor-pointer text-sm ease-in shadow-md bg-150 bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 hover:shadow-xs active:opacity-85 hover:-translate-y-px tracking-tight-rem bg-x-25"
-                                >
-                                    <i class="fas fa-plus"> </i>&nbsp;&nbsp;Add
-                                    New Data
-                                </Link>
-                            </div>
-
                             <div class="flex-auto px-0 pt-0 pb-2">
                                 <div class="p-0 overflow-x-auto">
                                     <div class="p-6 text-gray-900">
@@ -321,18 +273,23 @@ const submitCsv = () => {
                                                             class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
                                                         >
                                                             {{
-                                                                computers.computer.computer_code
+                                                                computers
+                                                                    .computer
+                                                                    .computer_code
                                                             }}
                                                         </p>
                                                     </td>
-                                                          <td
+                                                    <td
                                                         class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
                                                     >
                                                         <p
                                                             class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
                                                         >
                                                             {{
-                                                                computers.computer.pengguna.username
+                                                                computers
+                                                                    .computer
+                                                                    .pengguna
+                                                                    .username
                                                             }}
                                                         </p>
                                                     </td>
@@ -351,10 +308,50 @@ const submitCsv = () => {
                                                         class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
                                                     >
                                                         <span
-                                                            class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
+                                                            :class="
+                                                                getBadgeClassStatusInspeksi(
+                                                                    computers.inspection_status
+                                                                )
+                                                            "
                                                         >
                                                             {{
-                                                                computers.inspection_status
+                                                                getBadgeTextStatusInspeksi(
+                                                                    computers.inspection_status
+                                                                )
+                                                            }}
+                                                        </span>
+                                                    </td>
+                                                    <td
+                                                        class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                    >
+                                                        <span
+                                                            :class="
+                                                                getBadgeClassStatusFindings(
+                                                                    computers.findings_status
+                                                                )
+                                                            "
+                                                        >
+                                                            {{
+                                                                getBadgeTextStatusFindings(
+                                                                    computers.findings_status
+                                                                )
+                                                            }}
+                                                        </span>
+                                                    </td>
+                                                    <td
+                                                        class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                    >
+                                                        <span
+                                                            :class="
+                                                                getBadgeClassStatusInventory(
+                                                                    computers.inventory_status
+                                                                )
+                                                            "
+                                                        >
+                                                            {{
+                                                                getBadgeTextStatusInventory(
+                                                                    computers.inventory_status
+                                                                )
                                                             }}
                                                         </span>
                                                     </td>
@@ -365,31 +362,11 @@ const submitCsv = () => {
                                                             class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
                                                         >
                                                             {{
-                                                                computers.findings_status
+                                                                computers.ip_address
                                                             }}
                                                         </span>
                                                     </td>
                                                     <td
-                                                        class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
-                                                    >
-                                                        <span
-                                                            class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
-                                                        >
-                                                            {{
-                                                                computers.inventory_status
-                                                            }}
-                                                        </span>
-                                                    </td>
-                                                    <td
-                                                        class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
-                                                    >
-                                                        <span
-                                                            class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
-                                                        >
-                                                            {{ computers.ip_address }}
-                                                        </span>
-                                                    </td>
-                                                      <td
                                                         class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
                                                     >
                                                         <span
