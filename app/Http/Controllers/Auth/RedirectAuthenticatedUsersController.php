154d598f66eafd32865792f02241068c192259d1
dafaathaillah <dafabintang76@gmail.com>
2025-01-13 08:33:00 +0800

    update redirect login site

--- app/Http/Controllers/Auth/RedirectAuthenticatedUsersController.php
+++ app/Http/Controllers/Auth/RedirectAuthenticatedUsersController.php
@@ -10,12 +10,20 @@ class RedirectAuthenticatedUsersController extends Controller
     public function home(Request $request)
     {
         if (auth()->user()->role == 'ict_developer' && auth()->user()->site == 'BIB' || auth()->user()->role == 'ict_ho' && auth()->user()->site == 'HO' || auth()->user()->role == 'ict_bod' && auth()->user()->site == 'HO') {
-            // return redirect()->route('aduan.page');
                 return redirect('dashboard');
+        } elseif (auth()->user()->role == 'ict_technician' && auth()->user()->site == 'BA') {
+            return redirect()->route('dashboardBa.page');
+        } elseif (auth()->user()->role == 'ict_technician' && auth()->user()->site == 'MIFA') {
+            return redirect()->route('dashboardMifa.page');
+        } elseif (auth()->user()->role == 'ict_technician' && auth()->user()->site == 'MHU') {
+            return redirect()->route('dashboardMhu.page');
+        } elseif (auth()->user()->role == 'ict_technician' && auth()->user()->site == 'AMI') {
+            return redirect()->route('dashboardAmi.page');
+        } elseif (auth()->user()->role == 'ict_technician' && auth()->user()->site == 'WARA') {
+            return redirect()->route('dashboardWara.page');
         } elseif (auth()->user()->role == 'soc_ho') {
-            // return dd(auth()->user());
             return redirect()->route('aduan.page');
-        } elseif (auth()->user()->role == 'ict_section') {
+        }  elseif (auth()->user()->role == 'ict_section') {
             return redirect('sectionDashboard');
         } elseif (auth()->user()->role == 'ict_group_leader') {
             return redirect('groupLeaderDashboard');
