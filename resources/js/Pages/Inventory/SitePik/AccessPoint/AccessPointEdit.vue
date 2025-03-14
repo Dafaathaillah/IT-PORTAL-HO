7fc3dd8d44c02e613542163399b135eeb4110776
Dafaathaillah <dafabintang76@gmail.com>
2025-03-14 15:10:57 +0800

    update link cancel all site AP and Switch

--- resources/js/Pages/Inventory/SitePik/AccessPoint/AccessPointEdit.vue
+++ resources/js/Pages/Inventory/SitePik/AccessPoint/AccessPointEdit.vue
@@ -421,7 +421,7 @@ const save = () => {
                                 />
                                 <div class="flex flex-nowrap mt-6 justify-between">
                                     <Link
-                                        :href="route('accessPoint.page')"
+                                        :href="route('accessPointPik.page')"
                                         class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400"
                                     >
                                         <span
