64c0b8e84d843a90e25f23f12b632eefcd08b574
dikrix <dikriardiansyah@gmail.com>
2025-04-08 14:19:40 +0800

    hide dark mode

--- resources/js/Components/inventory/DashboardConfig.vue
+++ resources/js/Components/inventory/DashboardConfig.vue
@@ -9,8 +9,8 @@ const toggleDark = useToggle(isDark);
 
 watchEffect(() => {
     if (isDark.value === true) {
-        isDarkChecked.value = true;
-        localStorage.setItem('data-theme', 'dark');
+        // isDarkChecked.value = true;
+        // localStorage.setItem('data-theme', 'dark');
     }
 });
 
@@ -30,12 +30,12 @@ const configuratorClasses = computed(() =>
 
 <template>
     <div>
-        <a
+        <!-- <a
             @click="handleToggleConfig"
             class="fixed px-4 py-2 text-xl bg-white shadow-lg cursor-pointer bottom-8 right-8 z-990 rounded-circle text-slate-700"
         >
             <i class="py-2 pointer-events-none fa fa-cog"> </i>
-        </a>
+        </a> -->
         <!-- -right-90 in loc de 0-->
         <div
             class="z-sticky backdrop-blur-2xl backdrop-saturate-200 dark:bg-slate-850/80 shadow-3xl w-90 ease fixed top-0 left-auto flex h-full min-w-0 flex-col break-words rounded-none border-0 bg-white/80 bg-clip-border px-2.5 duration-200"
